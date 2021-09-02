bad7318a24a011800c7acea166e257135f5b62dc
Esaco <esaco@uninorte.edu.co>
2021-09-01 23:16:34 -0500

    New changes to web page.

--- Página Web/src/index.js
+++ Página Web/src/index.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const socket = require('socket.io');
 
 const server = http.createServer(app);
-var io = socket(server);
 
 app.use(express.static(__dirname + '/public/'));
 app.set('port', 3000);
 
-server.listen(app.get('port'), () => {
+server.listen(app.get('port'), "localhost", () => {
     console.log('Servidor web escuchando en el puerto 3000');
 
     function format_time(UNIX_timestamp) {
         const dtFormat = new Intl.DateTimeFormat('es-CO', {
             timeStyle: 'medium',
             dateStyle: 'full',
-            timeZone: 'Southamerica/Colombia'
+            timeZoneName: "GMT-5"
         });
         
         return dtFormat.format(new Date(UNIX_timestamp * 1000));
     }
+
+    /*
 
     // Base de datos.
     const mysql = require('mysql');
@@ -30,15 +30,17 @@ server.listen(app.get('port'), () => {
         user: 'root',
         password: '12345',
         database: 'app'
-    });
+    }); 
+    
+    */
 
     // Conexión a la base de datos.
     connection.connect((err) => {
-    if (err) {
-        console.log("No se pudo conectar a la base de datos.");
-        throw err
-    };
-    console.log('Base de datos conectada');
+        if (err) {
+            console.log("No se pudo conectar a la base de datos.");
+            throw err
+        };
+        console.log('Base de datos conectada.');
     });
 
     // Recibir datos del router.
@@ -49,7 +51,7 @@ server.listen(app.get('port'), () => {
 
         console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
 
-        const arr = msg.toString.split(";");
+        let arr = msg.toString.split(";");
         let latitud = arr[0];
         let longitud = arr[1];
         let timeStamp = arr[2];
@@ -61,41 +63,34 @@ server.listen(app.get('port'), () => {
         console.log(timeStamp);
         console.log(fecha);
 
-        io.emit('change', {
-            latitud_text: latitud,
-            longitud_text: longitud,
-            fecha_text: fecha,
-            hora_text: timeStamp
-        });
+        document.getElementById(latitud_text).innerText = latitud;
+        document.getElementById(longitud_text).innerText = longitud;
+        document.getElementById(fecha_text).innerText = fecha;
+        document.getElementById(hora_text).innerText = timeStamp;
 
-        io.on('connection', function(socket) {
-            socket.emit('change', {
-                latitud_text: latitud,
-                longitud_text: longitud,
-                fecha_text: fecha,
-                hora_text: timeStamp
-            });
-        });
-        
+        /*
+
         // Obtener el último dato.
         connection.query('SELECT MAX(ID) FROM datos', (err, rows) => {
-        if(err) {
-            console.log("No se pudo hacer la consulta.");
-            throw err
-        };
-        console.log(rows);
+            if(err) {
+                console.log("No se pudo hacer la consulta.");
+                throw err
+            };
+            console.log(rows);
         });
 
         // Insertar datos en la db.
         let id = 1;
         const insert_query = `INSERT INTO datos (Id, Latitud, Longitud, Fecha, Hora) VALUES(${id}, ${latitud}, ${longitud}, ${fecha}, ${hora})`;
         connection.query(insert_query, (err, rows) => {
-        if(err) {
-            console.log("No se pudo subir a la base de datos.");
-            throw err
-        };
-        console.log('Datos insertados en la base de datos.');
+            if(err) {
+                console.log("No se pudo subir a la base de datos.");
+                throw err
+            };
+            console.log('Datos insertados en la base de datos.');
         });
+
+        */
 
     });
 
