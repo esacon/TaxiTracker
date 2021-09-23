48496ce0a50d0000f56125370054d8ea7d102a59
Esaco <esaco@uninorte.edu.co>
2021-09-23 10:14:07 -0500

    Colors added.

--- Página Web/src/index.js
+++ Página Web/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-
+const colors = require('colors');
 
 const socket = require('socket.io');
 const env_var = require('dotenv').config();
