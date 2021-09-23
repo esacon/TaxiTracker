06647b986f14b2472242e035e5d807008202980a
Esaco <esaco@uninorte.edu.co>
2021-09-23 00:21:28 -0500

    Error fixed

--- Página Web/src/index.js
+++ Página Web/src/index.js
@@ -62,13 +62,6 @@ server.listen(app.get('port'), () => {
             latitud: latitud,
             longitud: longitud
         });
-
-        io.on('connection', function(socket) {
-            socket.emit('getData', {
-                latitud: latitud,
-                longitud: longitud
-            });
-        });
         
         console.log('Último dato recopilado con éxito.');
     });
