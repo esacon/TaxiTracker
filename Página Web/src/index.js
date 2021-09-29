cd69d2783239106b0c153acc03074fab130984ca
blondisb <blondisb@uninorte.edu.co>
2021-09-29 14:11:04 -0500

    Save and show data var.001

--- Página Web/src/index.js
+++ Página Web/src/index.js
@@ -17,9 +17,10 @@ var io = socket(server);
 var date;
 function setDate(val) {
     date = val;
-    document.getElementById("jsValue").value = yourVariable;
-    document.getElementById("seeYourVariable").innerHTML = yourVariable;
 }
+
+document.getElementById("jsValue").value = yourVariable;
+document.getElementById("seeYourVariable").innerHTML = yourVariable;
 
 
 
