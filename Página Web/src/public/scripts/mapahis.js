fbdd0cf8e879319b6ece478302205f52b74a4fc6
Diaza26 <dadrianac@uninorte.edu.co>
2021-09-30 01:24:39 -0500

    pruebahis

--- Página Web/src/public/scripts/mapahis.js
+++ Página Web/src/public/scripts/mapahis.js
@@ -1,4 +1,4 @@
-/*
+
 let marker;
 let map = L.map('maphi');            
 let polyline = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(map);
@@ -6,14 +6,13 @@ var lat = [10.554300, 10.554300, 10.554300, 10.554300, 10.554300, 10.554300, 10.
 var lon = [-73.224000, -73.224000, -73.224000, -73.224000, -73.224000, -73.224000, -73.222600, -73.222600, -73.222500, -73.222200, -73.222200, -73.222100, -73.221900, -73.221900, -73.221800, -73.221700];       
 var es=new L.LatLng(lat,lon);
 var polyline = L.polyline(es).addTo(map);   
-*/
+
 // Update HTML content
 document.addEventListener('DOMContentLoaded', function() {
- //polyline.addTo(map);
+    polyline.addTo(map);
 
     // Load Map
-    var map = L.map('maphi').setView([46, 14.84], 7); 
-
+    map.setView(es, 18); 
     L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=mAWo6ZVOwQECEfInDbLo', {
             attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
             maxZoom: 20,
@@ -21,20 +20,11 @@ document.addEventListener('DOMContentLoaded', function() {
             zoomOffset: -1,
         }).addTo(map);   
 
-        var lineStrings = turf.randomLineString(10, {bbox: [13.24, 45.5, 16.35, 46.84], num_vertices: 100000})
+    // Place first marker
+    marker = L.marker(es).addTo(map);                         
 
-        var vectorGrid = L.vectorGrid.slicer(lineStrings, {
-          maxZoom: 18,
-          rendererFactory: L.svg.tile,
-          vectorTileLayerStyles: {
-            sliced: function(properties, zoom) {
-              return {
-                stroke: true,
-                color: 'red',
-                weight: 2
-              }
-            }
-          },
-          interactive: false,
-        }).addTo(map);
+    // Initialize map.
+    map.setView(es);
+    marker.setLatLng(es);                        
+    polyline.addLatLng(es);
 }); 
