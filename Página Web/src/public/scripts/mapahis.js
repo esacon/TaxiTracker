d471c5f035a6daf75800e5f06c7505bce5b4b7df
Diaza26 <dadrianac@uninorte.edu.co>
2021-09-29 23:15:20 -0500

    pruebahis

--- Página Web/src/public/scripts/mapahis.js
+++ Página Web/src/public/scripts/mapahis.js
@@ -5,7 +5,7 @@ let map = L.map('maphi');
 let polyline = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(map);
 let lat=[10.554300, 10.554300, 10.554300, 10.554300, 10.554300, 10.554300, 10.556000, 10.556000, 10.556200, 10.556400, 10.556400, 10.556600, 10.556900, 10.556900, 10.557200, 10.557500, 10.557500]
 let lon=[-73.224000, -73.224000, -73.224000, -73.224000, -73.224000, -73.224000, -73.222600, -73.222600, -73.222500, -73.222200, -73.222200, -73.222100, -73.221900, -73.221900, -73.221800, -73.221700]
-var polylinePoints = [lat,lon];            
+var polylinePoints = [parseFloat(lat),parseFloat(lon)];            
   
   var polyline = L.polyline(polylinePoints).addTo(map);   
 
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
     polyline.addTo(map);
 
     // Load Map
-    map.setView([lat,lon], 18); 
+    map.setView([parseFloat(lat),parseFloat(lon)], 18); 
     L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=mAWo6ZVOwQECEfInDbLo', {
             attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
             maxZoom: 20,
@@ -23,10 +23,10 @@ document.addEventListener('DOMContentLoaded', function() {
         }).addTo(map);   
 
     // Place first marker
-    marker = L.marker([lat,lon]).addTo(map);                         
+    marker = L.marker([parseFloat(lat),parseFloat(lon)]).addTo(map);                         
 
     // Initialize map.
-    map.setView([lat,lon]);
-    marker.setLatLng([lat,lon]);                        
-    polyline.addLatLng([lat,lon]);
+    map.setView([parseFloat(lat),parseFloat(lon)]);
+    marker.setLatLng([parseFloat(lat),parseFloat(lon)]);                        
+    polyline.addLatLng([parseFloat(lat),parseFloat(lon)]);
 }); 
