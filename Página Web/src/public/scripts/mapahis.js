d88e71793b87cbe6a03ed5a3a3d91bf84cda86fb
Diaza26 <dadrianac@uninorte.edu.co>
2021-09-30 00:02:48 -0500

    pruebahis

--- Página Web/src/public/scripts/mapahis.js
+++ Página Web/src/public/scripts/mapahis.js
@@ -2,17 +2,17 @@
 let marker;
 let map = L.map('maphi');            
 let polyline = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(map);
-var lat = new L.LatLng(10.554300, 10.554300, 10.554300, 10.554300, 10.554300, 10.554300, 10.556000, 10.556000, 10.556200, 10.556400, 10.556400, 10.556600, 10.556900, 10.556900, 10.557200, 10.557500, 10.557500);
-var lon = new L.LatLng(-73.224000, -73.224000, -73.224000, -73.224000, -73.224000, -73.224000, -73.222600, -73.222600, -73.222500, -73.222200, -73.222200, -73.222100, -73.221900, -73.221900, -73.221800, -73.221700);       
-  
-var polyline = L.polyline([lat, lon]).addTo(map);   
+var lat = [10.554300, 10.554300, 10.554300, 10.554300, 10.554300, 10.554300, 10.556000, 10.556000, 10.556200, 10.556400, 10.556400, 10.556600, 10.556900, 10.556900, 10.557200, 10.557500, 10.557500];
+var lon = [-73.224000, -73.224000, -73.224000, -73.224000, -73.224000, -73.224000, -73.222600, -73.222600, -73.222500, -73.222200, -73.222200, -73.222100, -73.221900, -73.221900, -73.221800, -73.221700];       
+var es=new L.LatLng(lat,lon);
+var polyline = L.polyline(es).addTo(map);   
 
 // Update HTML content
 document.addEventListener('DOMContentLoaded', function() {
     polyline.addTo(map);
 
     // Load Map
-    map.setView([lat,lon], 18); 
+    map.setView(es, 18); 
     L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=mAWo6ZVOwQECEfInDbLo', {
             attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
             maxZoom: 20,
@@ -21,10 +21,10 @@ document.addEventListener('DOMContentLoaded', function() {
         }).addTo(map);   
 
     // Place first marker
-    marker = L.marker([lat, lon]).addTo(map);                         
+    marker = L.marker(es).addTo(map);                         
 
     // Initialize map.
-    map.setView([lat, lon]);
-    marker.setLatLng([lat, lon]);                        
-    polyline.addLatLng([lat, lon]);
+    map.setView(es);
+    marker.setLatLng(es);                        
+    polyline.addLatLng(es);
 }); 
