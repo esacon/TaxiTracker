666068f0fe4e154a38440b560eada511b389803d
Esaco <esaco@uninorte.edu.co>
2021-09-30 08:18:20 -0500

    Mapa implementado

--- Página Web/src/public/scripts/mapahis.js
+++ Página Web/src/public/scripts/mapahis.js
@@ -2,25 +2,14 @@
 var socket = io();
 let marker1;
 let marker2;
-let map = L.map('maphi');  
-map.setView([10.97, -74.65], 15); 
-L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=mAWo6ZVOwQECEfInDbLo', {
-        attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-        maxZoom: 20,
-        center: [10.97, -74.65],
-        tileSize: 512,
-        zoomOffset: -1,
-    }).addTo(map);      
+let map = L.map('maphi').setView([10.97, -74.65], 15);      
 let polyline = L.polyline([], {color: '#41b611', smoothFactor:3});
 
 // Update HTML content
 document.addEventListener('DOMContentLoaded', function() { 
 
     socket.on("getConsulta", function(data){
-        const info = data.info
-        polyline.remove();
-        marker1.remove();
-        marker2.remove();
+        const info = data.info;
 
         // Initialize map.
         const inicio = [parseFloat(info[0].Latitud), parseFloat(info[0].Longitud)];
@@ -28,17 +17,20 @@ document.addEventListener('DOMContentLoaded', function() {
         const medio = [parseFloat(info[Math.floor(info.length/2)].Latitud), parseFloat(info[Math.floor(info.length/2)].Longitud)];
         
         // Load Map
-        map.setView(medio);    
+        map.setView(medio); 
+        L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=mAWo6ZVOwQECEfInDbLo', {
+            attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+            maxZoom: 20,
+            center: medio,
+            tileSize: 512,
+            zoomOffset: -1,
+        }).addTo(map);    
 
         // Place markers
-        polyline = L.polyline([], {color: '#41b611', smoothFactor:3});
-        polyline.addTo(map);
-
-        marker1 = L.marker(inicio).addTo(map);
-        marker1.bindPopup("<b>Punto de inicio</b>").openPopup(); 
-        marker2 = L.marker(fin).addTo(map); 
-        marker2.bindPopup("<b>Punto de fin</b>").openPopup();
-
+        marker1 = L.marker(inicio).addTo(map).bindPopup("<b>Punto de inicio</b>").openPopup(); 
+        marker2 = L.marker(fin).addTo(map).bindPopup("<b>Punto de fin</b>").openPopup();
+    
+        polyline = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(map);
         info.forEach(coord => {
             polyline.addLatLng([parseFloat(coord.Latitud), parseFloat(coord.Longitud)]);
         }); 
