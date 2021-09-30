c50b09357d62af649e0bc63fd0cbcda2d57a82f4
Esaco <esaco@uninorte.edu.co>
2021-09-30 03:44:33 -0500

    Mapa implementado

--- Página Web/src/public/scripts/mapahis.js
+++ Página Web/src/public/scripts/mapahis.js
@@ -1,37 +1,34 @@
 
-                var socket = io();
-                let marker;
-                let circle;
-                let map = L.map('maphi');            
-                let polyline = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(map);
-                // Load Map
-                map.setView([0,0], 18); 
-                L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=mAWo6ZVOwQECEfInDbLo', {
-                        attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-                        maxZoom: 20,
-                        center: [0,0],
-                        tileSize: 512,
-                        zoomOffset: -1,
-                    }).addTo(map);   
-                
+var socket = io();
+let marker;
+let map = L.map('maphi');            
+let polyline = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(map);
+// Load Map
+map.setView([0,0], 18);   
 
-                // Update HTML content
-                document.addEventListener('DOMContentLoaded', function() {         
-                    socket.on("getConsulta", function(data){
-                        const info = data.info;
-                        // Initialize map.
-                        const inicio = [parseFloat(info[0]['latitud']), parseFloat(info[0]['longitud'])];
-                        const fin = [parseFloat(info[info.length - 1]['latitud']), parseFloat(info[info.length - 1]['longitud'])];
-                        const medio = [parseFloat(info[Math.floor(info.length/2)]['latitud']), parseFloat(info[Math.floor(info.length/2)]['longitud'])];
-                        
-                        // Place first marker
-                        marker = L.marker(medio).addTo(map); 
+// Update HTML content
+document.addEventListener('DOMContentLoaded', function() { 
 
-                        marker.setLatLng(inicio); 
-                        marker.setLatLng(fin);  
+    socket.on("getConsulta", function(data){
+        const info = data.info
 
-                        info.forEach(coord => {
-                            polyline.addLatLng([parseFloat(info[coord]['latitud']), parseFloat(info[coord]['longitud'])]);
-                        }); 
-                    });
-                }); 
+        // Initialize map.
+        const inicio = [parseFloat(info[0].Latitud), parseFloat(info[0].Longitud)];
+        const fin = [parseFloat(info[info.length - 1].Latitud), parseFloat(info[info.length - 1].Longitud)];
+        const medio = [parseFloat(info[Math.floor(info.length/2)].Latitud), parseFloat(info[Math.floor(info.length/2)].Longitud)];
+        L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=mAWo6ZVOwQECEfInDbLo', {
+                attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+                maxZoom: 20,
+                center: medio,
+                tileSize: 512,
+                zoomOffset: -1,
+            }).addTo(map);   
+        // Place first marker
+        marker = L.marker(inicio).addTo(map); 
+        marker.setLatLng(fin);  
+
+        info.forEach(coord => {
+            polyline.addLatLng([parseFloat(coord.Latitud), parseFloat(coord.Longitud)]);
+        }); 
+    });
+}); 
