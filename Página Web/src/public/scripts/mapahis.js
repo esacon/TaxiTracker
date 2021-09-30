dbc77781d9e2f830b9a4bcb41e8d468b8666edfa
Esaco <esaco@uninorte.edu.co>
2021-09-30 09:54:33 -0500

    Mapa implementado

--- Página Web/src/public/scripts/mapahis.js
+++ Página Web/src/public/scripts/mapahis.js
@@ -10,30 +10,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
     socket.on("getConsulta", function(data){
         const info = data.info;
-        map.removeLayer([marker1, marker2, polyline]);
+        if(info.length != 0) {
+            console.log(info);
+            map.removeLayer([marker1, marker2, polyline]);
 
-        // Initialize map.
-        const inicio = [parseFloat(info[0].Latitud), parseFloat(info[0].Longitud)];
-        const fin = [parseFloat(info[info.length - 1].Latitud), parseFloat(info[info.length - 1].Longitud)];
-        const medio = [parseFloat(info[Math.floor(info.length/2)].Latitud), parseFloat(info[Math.floor(info.length/2)].Longitud)];
+            // Initialize map.
+            const inicio = [parseFloat(info[0].Latitud), parseFloat(info[0].Longitud)];
+            const fin = [parseFloat(info[info.length - 1].Latitud), parseFloat(info[info.length - 1].Longitud)];
+            const medio = [parseFloat(info[Math.floor(info.length/2)].Latitud), parseFloat(info[Math.floor(info.length/2)].Longitud)];
+            
+            // Load Map
+            map.setView(medio); 
+            L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=mAWo6ZVOwQECEfInDbLo', {
+                attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+                maxZoom: 20,
+                center: medio,
+                tileSize: 512,
+                zoomOffset: -1,
+            }).addTo(map);    
+
+            // Place markers
+            marker1 = L.marker(inicio).addTo(map).bindPopup("<b>Punto de inicio</b>").openPopup(); 
+            marker2 = L.marker(fin).addTo(map).bindPopup("<b>Punto de fin</b>").openPopup();
         
-        // Load Map
-        map.setView(medio); 
-        L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=mAWo6ZVOwQECEfInDbLo', {
-            attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-            maxZoom: 20,
-            center: medio,
-            tileSize: 512,
-            zoomOffset: -1,
-        }).addTo(map);    
-
-        // Place markers
-        marker1 = L.marker(inicio).addTo(map).bindPopup("<b>Punto de inicio</b>").openPopup(); 
-        marker2 = L.marker(fin).addTo(map).bindPopup("<b>Punto de fin</b>").openPopup();
-    
-        polyline = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(map);
-        info.forEach(coord => {
-            polyline.addLatLng([parseFloat(coord.Latitud), parseFloat(coord.Longitud)]);
-        }); 
+            polyline = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(map);
+            info.forEach(coord => {
+                polyline.addLatLng([parseFloat(coord.Latitud), parseFloat(coord.Longitud)]);
+            }); 
+        }
     });
 }); 
