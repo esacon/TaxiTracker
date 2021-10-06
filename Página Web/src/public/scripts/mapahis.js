b4d5fb7621be0e0d62d255d8995a7538fa71bb1e
Diaza26 <dadrianac@uninorte.edu.co>
2021-10-06 13:24:09 -0500

    pruebapolilinea

--- Página Web/src/public/scripts/mapahis.js
+++ Página Web/src/public/scripts/mapahis.js
@@ -2,7 +2,7 @@
 var socket = io();
 let marker1;
 let marker2;
-let map = L.map('maphi').setView([10.97, -74.65], 15);      
+let maphi= L.map('maphi').setView([10.97, -74.65], 15);      
 let polyline = L.polyline([], {color: '#41b611', smoothFactor:3});
 
 // Update HTML content
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const info = data.info;
         if(info.length != 0) {
             console.log(info);
-            map.removeLayer([marker1, marker2, polyline]);
+            maphi.removeLayer([marker1, marker2, polyline]);
 
             // Initialize map.
             const inicio = [parseFloat(info[0].Latitud), parseFloat(info[0].Longitud)];
@@ -20,18 +20,18 @@ document.addEventListener('DOMContentLoaded', function() {
             const medio = [parseFloat(info[Math.floor(info.length/2)].Latitud), parseFloat(info[Math.floor(info.length/2)].Longitud)];
             
             // Load Map
-            map.setView(medio); 
+            maphi.setView(medio); 
             L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=mAWo6ZVOwQECEfInDbLo', {
                 attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
                 maxZoom: 20,
                 center: medio,
                 tileSize: 512,
                 zoomOffset: -1,
-            }).addTo(map);    
+            }).addTo(maphi);    
 
             // Place markers
-            marker1 = L.marker(inicio).addTo(map).bindPopup("<b>Punto de inicio</b>").openPopup(); 
-            marker2 = L.marker(fin).addTo(map).bindPopup("<b>Punto de fin</b>").openPopup();
+            marker1 = L.marker(inicio).addTo(maphi).bindPopup("<b>Punto de inicio</b>").openPopup(); 
+            marker2 = L.marker(fin).addTo(maphi).bindPopup("<b>Punto de fin</b>").openPopup();
             polyline = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(maphi);
             info.forEach(coord => {  
                 polyline.addLatLng([parseFloat(coord.Latitud), parseFloat(coord.Longitud)]);
