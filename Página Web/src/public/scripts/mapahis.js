4213ccfb72a2a9b66acdf82b3ac6ab786f9b2564
Diaza26 <dadrianac@uninorte.edu.co>
2021-10-06 16:24:33 -0500

    responsive

--- Página Web/src/public/scripts/mapahis.js
+++ Página Web/src/public/scripts/mapahis.js
@@ -21,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
             
             // Load Map
             maphi.setView(medio); 
+            
             L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=mAWo6ZVOwQECEfInDbLo', {
                 attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
                 maxZoom: 20,
