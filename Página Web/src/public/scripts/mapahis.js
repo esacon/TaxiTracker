0790fb7ae0268cb560303826cc9e0a0203f59165
Diaza26 <dadrianac@uninorte.edu.co>
2021-10-06 13:27:45 -0500

    pruebapolilinea

--- Página Web/src/public/scripts/mapahis.js
+++ Página Web/src/public/scripts/mapahis.js
@@ -33,7 +33,6 @@ document.addEventListener('DOMContentLoaded', function() {
             marker1 = L.marker(inicio).addTo(maphi).bindPopup("<b>Punto de inicio</b>").openPopup(); 
             marker2 = L.marker(fin).addTo(maphi).bindPopup("<b>Punto de fin</b>").openPopup();
             polyline = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(maphi);
-            polyline.removeFrom(maphi);
             info.forEach(coord => {  
                 polyline.addLatLng([parseFloat(coord.Latitud), parseFloat(coord.Longitud)]);
             }); 
