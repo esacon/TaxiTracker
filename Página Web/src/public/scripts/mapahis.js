1e495cb9fe1dfb8f13ad06515f521a11a079cc16
Esaco <esaco@uninorte.edu.co>
2021-10-07 10:52:27 -0500

    Prueba poli

--- Página Web/src/public/scripts/mapahis.js
+++ Página Web/src/public/scripts/mapahis.js
@@ -43,21 +43,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
         let fechas = [];
         let horas = [];
-        let longitudes = [];
+        let coords = [];
         info.forEach(coord => {  
             polyline.addLatLng([parseFloat(coord.Latitud), parseFloat(coord.Longitud)]);
-            longitudes.push(parseFloat(coord.Longitud));
+            coords.push([parseFloat(coord.Latitud), parseFloat(coord.Longitud)]);
             horas.push(coord.Hora);
             fechas.push(coord.Fecha);
         }); 
 
-        polyline.on('click', (e) => {
-            const target = parseFloat(e.latlng.lng);
-            let cercano = longitudes.reduce(
+        polyline.on('click', function(e) {
+                                    const target = parseFloat(e.latlng);
+                                    console.log(target)
+                                    let cercano = coords.reduce(
                             function(prev, curr) {
                                 return (Math.abs(curr - target) < Math.abs(prev - target) ? curr : prev);
                             });
-            const index = longitudes.indexOf(cercano);
+            const index = coords.indexOf(cercano);
             let label = `<b>Taxi ubicado en:</b><br/>Latitud: ${e.latlng.lat}<br/>Longitud: ${e.latlng.lng}<br/>Fecha: ${fechas[index]}<br/>Hora: ${horas[index]}`
             popup.setLatLng(e.latlng).setContent(label).openOn(maphi);
         });  
