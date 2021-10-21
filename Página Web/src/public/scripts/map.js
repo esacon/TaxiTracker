cb40eda400c751d7b47401b59ce32eafeae912d9
Esaco <esaco@uninorte.edu.co>
2021-10-21 02:04:34 -0500

    Avrrr

--- Página Web/src/public/scripts/map.js
+++ Página Web/src/public/scripts/map.js
@@ -121,17 +121,19 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         
         if(p1) {
-            polyline.removeFrom(map);
-            map.removeLayer(polyline);
             polyline = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(map);
             polyline.setLatLngs(coord_taxi1); 
+        } else {            
+            polyline.removeFrom(map);
+            map.removeLayer(polyline);
         }
 
         if(p2) {
+            polyline2.setLatLngs(coord_taxi2); 
+        } else{            
+            polyline2 = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(map);
             polyline2.removeFrom(map);
             map.removeLayer(polyline2);
-            polyline2 = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(map);
-            polyline2.setLatLngs(coord_taxi2); 
-        }  
+        }
     });
 }); 
