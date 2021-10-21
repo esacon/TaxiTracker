78b7e432de7ae76af80b77ecf21a2b15f02f31a3
Esaco <esaco@uninorte.edu.co>
2021-10-20 20:40:34 -0500

    Slider value first try

--- Página Web/src/public/scripts/mapahis.js
+++ Página Web/src/public/scripts/mapahis.js
@@ -68,17 +68,17 @@ document.addEventListener('DOMContentLoaded', function() {
             fechas.push(coord.Fecha);
         });  
 
-        let $avanzar = document.querySelector('#rango');        
+        let $avanzar = document.querySelector('#rango');    
+        $avanzar.setAttribute("max", coords.length);
+
         $avanzar.addEventListener('click', ()=> {
             if (marker3 != undefined) {
                 maphi.removeLayer(marker3);
             };
-        
-            if (index != 0) {
-                index--;
-                let label = `<b>Taxi ubicado en:</b><br/>Latitud: ${coords[index][0]}<br/>Longitud: ${coords[index][1]}<br/>Fecha: ${fechas[index]}<br/>Hora: ${horas[index]}`
-                marker3 = L.marker([coords[index][0], coords[index][1]], {icon: taxiIcon}).addTo(maphi).bindPopup(label).openPopup();
-            } 
+
+            index = $avanzar.value;
+            let label = `<b>Taxi ubicado en:</b><br/>Latitud: ${coords[index][0]}<br/>Longitud: ${coords[index][1]}<br/>Fecha: ${fechas[index]}<br/>Hora: ${horas[index]}`
+            marker3 = L.marker([coords[index][0], coords[index][1]], {icon: taxiIcon}).addTo(maphi).bindPopup(label).openPopup();
         });
     });
 
