c78818da84cfb3ed76e8882dacab63fcb3af7415
Esaco <esaco@uninorte.edu.co>
2021-10-21 15:05:46 -0500

    Correcciones añadidas.

--- Página Web/src/main.js
+++ Página Web/src/main.js
@@ -46,28 +46,41 @@ router.post('/historicos', (req, res) => {
     console.log([start_date, start_hour, end_date, end_hour]);
 
     async function retrieve() {
-        let info;
-        let info2 = null;
-        
+        let info, info2;
         if (body.placa === '0'){
+            info2 = null;
             info = await database.getData(`SELECT * FROM datos WHERE str_to_date(concat(fecha, ' ', hora),'%Y-%m-%d %H:%i:%s') >= str_to_date(concat('${start_date}', ' ', '${start_hour}'),'%Y-%m-%d %H:%i:%s') AND str_to_date(concat(fecha, ' ', hora),'%Y-%m-%d %H:%i:%s') <= str_to_date(concat('${end_date}', ' ', '${end_hour}'),'%Y-%m-%d %H:%i:%s') and placa = 'AAA111'`);
         } else if (body.placa === '1'){
             info = await database.getData(`SELECT * FROM datos WHERE str_to_date(concat(fecha, ' ', hora),'%Y-%m-%d %H:%i:%s') >= str_to_date(concat('${start_date}', ' ', '${start_hour}'),'%Y-%m-%d %H:%i:%s') AND str_to_date(concat(fecha, ' ', hora),'%Y-%m-%d %H:%i:%s') <= str_to_date(concat('${end_date}', ' ', '${end_hour}'),'%Y-%m-%d %H:%i:%s') and placa = 'AAA222'`); 
+            info2 = null;
         } else {
             info = await database.getData(`SELECT * FROM datos WHERE str_to_date(concat(fecha, ' ', hora),'%Y-%m-%d %H:%i:%s') >= str_to_date(concat('${start_date}', ' ', '${start_hour}'),'%Y-%m-%d %H:%i:%s') AND str_to_date(concat(fecha, ' ', hora),'%Y-%m-%d %H:%i:%s') <= str_to_date(concat('${end_date}', ' ', '${end_hour}'),'%Y-%m-%d %H:%i:%s') and placa = 'AAA111'`);
             info2 = await database.getData(`SELECT * FROM datos WHERE str_to_date(concat(fecha, ' ', hora),'%Y-%m-%d %H:%i:%s') >= str_to_date(concat('${start_date}', ' ', '${start_hour}'),'%Y-%m-%d %H:%i:%s') AND str_to_date(concat(fecha, ' ', hora),'%Y-%m-%d %H:%i:%s') <= str_to_date(concat('${end_date}', ' ', '${end_hour}'),'%Y-%m-%d %H:%i:%s') and placa = 'AAA222'`); 
         }
 
         io.on('connection', function(socket) {
-            if (info.length != 0) {
-                socket.emit('getConsulta', {
-                    info: info,
-                    info2: info2
-                });
+            if (info2 === null) {
+                if (info.length != 0) {
+                    socket.emit('getConsulta', {
+                        info: info,
+                        info2: null
+                    });
+                } else {
+                    socket.emit('noData', {
+                        info: info
+                    });
+                }
             } else {
-                socket.emit('noData', {
-                    info: info
-                });
+                if (info.length != 0) {
+                    socket.emit('getConsulta', {
+                        info: info,
+                        info2: info2
+                    });
+                } else {
+                    socket.emit('noData', {
+                        info: info
+                    });
+                }
             }
         });        
     }; 
