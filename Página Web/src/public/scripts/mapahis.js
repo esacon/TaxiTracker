55bf6828bdc81c840d105b15ffdebb750ef50175
Diaza26 <dadrianac@uninorte.edu.co>
2021-11-10 09:04:27 -0500

    dos carros al tiempo y dia en la busqueda

--- Página Web/src/public/scripts/mapahis.js
+++ Página Web/src/public/scripts/mapahis.js
@@ -31,78 +31,6 @@ document.addEventListener('DOMContentLoaded', function() {
     socket.on("getConsulta", function(data){
         let info = data.info;
         let info2 = data.info2;
-
-        if (info2 === null) {                  
-            maphi.removeLayer([marker1, marker2, marker3, marker4, marker5, marker6, polyline, polyline2]);
-
-            // Initialize map.
-            const inicio = [parseFloat(info[0].Latitud), parseFloat(info[0].Longitud)];
-            const fin = [parseFloat(info[info.length - 1].Latitud), parseFloat(info[info.length - 1].Longitud)];
-            const medio = [parseFloat(info[Math.floor(info.length/2)].Latitud), parseFloat(info[Math.floor(info.length/2)].Longitud)];
-            
-            // Load Map
-            maphi.setView(medio); 
-            
-            L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=mAWo6ZVOwQECEfInDbLo', {
-                attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-                maxZoom: 20,
-                center: medio,
-                tileSize: 512,
-                zoomOffset: -1
-            }).addTo(maphi);    
-
-            // Place markers
-            if (marker1 != undefined) {
-                maphi.removeLayer(marker1);
-            };
-            if (marker2 != undefined) {
-                maphi.removeLayer(marker2);
-            };
-            if (marker3 != undefined) {
-                maphi.removeLayer(marker3);
-            };
-            if (marker4 != undefined) {
-                maphi.removeLayer(marker4);
-            };
-            if (marker5 != undefined) {
-                maphi.removeLayer(marker5);
-            };
-            if (marker6 != undefined) {
-                maphi.removeLayer(marker6);
-            };
-
-            polyline2.removeFrom(maphi);
-            polyline2 = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(maphi);
-            marker1 = L.marker(inicio).addTo(maphi).bindPopup("<b>Punto de inicio</b>").openPopup();             
-            marker2 = L.marker(fin).addTo(maphi).bindPopup("<b>Punto de fin</b>").openPopup();
-            polyline.removeFrom(maphi);
-            polyline = L.polyline([], {color: '#41b611', smoothFactor:3}).addTo(maphi);
-
-            let fechas = [];
-            let horas = [];
-            let coords = [];
-            let rpms = [];
-            info.forEach(coord => {  
-                polyline.addLatLng([parseFloat(coord.Latitud), parseFloat(coord.Longitud)]);
-                coords.push([parseFloat(coord.Latitud), parseFloat(coord.Longitud)]);
-                horas.push(coord.Hora);
-                fechas.push(coord.Fecha);
-                rpms.push(coord.RPM);
-            });  
-
-            let $avanzar = document.querySelector('#rango');    
-            $avanzar.setAttribute("max", coords.length - 1);
-
-            $avanzar.addEventListener('change', ()=> {
-                if (marker3 != undefined) {
-                    maphi.removeLayer(marker3);
-                };
-
-                index = $avanzar.value;
-                let label = `<b>Taxi ubicado en:</b><br/>Latitud: ${coords[index][0]}<br/>Longitud: ${coords[index][1]}<br/>Fecha: ${fechas[index]}<br/>Hora: ${horas[index]}<br/>RPM: ${rpms[index]}`
-                marker3 = L.marker([coords[index][0], coords[index][1]], {icon: taxiIcon}).addTo(maphi).bindPopup(label).openPopup();
-            });
-        } else {
             maphi.removeLayer([marker1, marker2, marker3, marker4, marker5, marker6, polyline, polyline2]);
 
             // Initialize map.
@@ -201,7 +129,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 let label2 = `<b>Taxi AAA222 ubicado en:</b><br/>Latitud: ${coords2[index2][0]}<br/>Longitud: ${coords2[index2][1]}<br/>Fecha: ${fechas2[index2]}<br/>Hora: ${horas2[index2]}<br/>RPM: ${rpms2[index]}`
                 marker6 = L.marker([coords2[index2][0], coords2[index2][1]], {icon: taxiIcon}).addTo(maphi).bindPopup(label2).openPopup();
             });
-        }
     });
 
     socket.on('noData', function(data){
